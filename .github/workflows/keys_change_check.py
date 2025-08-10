8b983ef4c60e469da17be27ea1b1d71b419e1df9
Ríša Szlachta <risa.szlachta@gmail.com>
2025-08-10 15:08:15 +0200

    Update keys_change_check.py

--- .github/workflows/keys_change_check.py
+++ .github/workflows/keys_change_check.py
@@ -4,6 +4,6 @@ import os
 
 result_file = open(os.environ['GITHUB_OUTPUT'], 'a')
 
-result_file.write("keys_change_check.py result.md file output")
+result_file.write("\n\nkeys_change_check.py result.md file output\n\n")
 
 result_file.close()
