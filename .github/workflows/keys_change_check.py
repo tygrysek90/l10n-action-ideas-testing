9534338c3907b6c4b32f06f9fecf4e93c073195a
Ríša Szlachta <risa.szlachta@gmail.com>
2025-08-10 16:06:32 +0200

    Update keys_change_check.py

--- .github/workflows/keys_change_check.py
+++ .github/workflows/keys_change_check.py
@@ -1,10 +1,17 @@
-import imp
+import importlib.util
 
 print("keys_change_check.py script text output")
 
 module_path = 'pr/.github/workflows/translation_check.py'
 
-keys_pr_module = imp.load_source('keyspr', module_path)
+module_name = 'keys_pr'  # You can choose any name for the module
+
+# Create a spec for the module
+module_spec = importlib.util.spec_from_file_location(module_name, module_path)
+
+# Load the module from the spec
+keys_pr_module = importlib.util.module_from_spec(module_spec)
+module_spec.loader.exec_module(gfg_module)
 
 print(keys_pr_module.KEYS_TO_IGNORE)
 
