611206582587d7995814203d65c1417160420ce6
Ríša Szlachta <risa.szlachta@gmail.com>
2025-08-10 16:01:12 +0200

    Update keys_change_check.py

--- .github/workflows/keys_change_check.py
+++ .github/workflows/keys_change_check.py
@@ -1,4 +1,12 @@
+import imp
+
 print("keys_change_check.py script text output")
+
+module_path = 'pr/.github/workflows/translation_check.py'
+
+keys_pr_module = imp.load_source('keyspr', module_path)
+
+print(keys_pr_module.KEYS_TO_IGNORE)
 
 result = '#### Check results\n\n'
 
