dae4730922e3f9782d89909def470f82ea7fc0c0
tygrysek90 <risa.szlachta@gmail.com>
2025-08-10 20:48:40 +0200

    niceee

--- .github/workflows/keys_change_check.py
+++ .github/workflows/keys_change_check.py
@@ -2,62 +2,81 @@ import importlib.util
 
 print("keys_change_check.py script text output")
 
-module_path = 'pr/.github/workflows/translation_check.py'
-module_name = 'keys_pr'  # You can choose any name for the module
+"""
+Load modules to compare and en-GB.txt file
+"""
+#module_path = 'pr/.github/workflows/translation_check.py'
+module_path = '/home/richard/openrct2-workshop/staged/pr/translation_check.py'
+module_name = 'keys_pr'  
 module_spec = importlib.util.spec_from_file_location(module_name, module_path)
 keys_pr_module = importlib.util.module_from_spec(module_spec)
 module_spec.loader.exec_module(keys_pr_module)
 
-module_path = 'master/.github/workflows/translation_check.py'
-module_name = 'keys_master'  # You can choose any name for the module
+#module_path = 'master/.github/workflows/translation_check.py'
+module_path = '/home/richard/openrct2-workshop/staged/master/translation_check.py'
+module_name = 'keys_master'  
 module_spec = importlib.util.spec_from_file_location(module_name, module_path)
 keys_master_module = importlib.util.module_from_spec(module_spec)
 module_spec.loader.exec_module(keys_master_module)
 
-
-# takes list of STR_nnnn in keys_list_old, compares to keys_list_new_unsorted
-# takes data from en-GB.txt
-# produces diff-style output
-# produces new list ready to be inserted into code in translation_check.py
-
-# by tygrysek90, enjoy :)
-
 keys_list_old = keys_master_module.KEYS_TO_IGNORE
 keys_list_new = keys_pr_module.KEYS_TO_IGNORE
 
-keys = keys_list_new.copy()
-# use github codeblock diff style
+#OPENRCT2_EN_GB_FILE = "OpenRCT2/data/language/en-GB.txt"
+OPENRCT2_EN_GB_FILE = "/home/richard/openrct2-workshop/staged/en-GB.txt"
+
 result_file = open("result.md", "w")
 
-result = ("```diff")
+result = "**Comparing changes**\n\n"
+result += "`KEYS_TO_IGNORE` in `translation_check.py` - list items expanded using `develop` version of `en-GB.txt`\n\n"
 
-OPENRCT2_EN_GB_FILE = "OpenRCT2/data/language/en-GB.txt"
+result += "```diff\n"
+"""
+use github codeblock diff style
+"""
 
+counter_add = 0
+counter_rem = 0
 
-for line in open(OPENRCT2_EN_GB_FILE ):
+for line in open(OPENRCT2_EN_GB_FILE):
     if line[0:8] in keys_list_old and line[0:8] in keys_list_new:
         result += ("  "+line) # =
     if line[0:8] in keys_list_old and line[0:8] not in keys_list_new:
         result += ("- "+line)
+        counter_rem += 1
     if line[0:8] not in keys_list_old and line[0:8] in keys_list_new:
         result += ("+ "+line)
+        counter_add += 1
     if line[0:8] in keys_list_new:
         keys_list_new.remove(line[0:8])
+    if line[0:8] in keys_list_old:
+        keys_list_old.remove(line[0:8])
 
 result += "\n"
 
-result += (" "+20*"-")
-result += "\n"
-result += ("KEYS NOT FOUND IN en-GB.txt")
-result += "\n"
+result += " "+30*"-"+"\n"
 
-for key in keys_list_new:
-    result += ("! "+key)
+result += "+ "+str(counter_add)+" keys\n"
+result += "- "+str(counter_rem)+" keys\n"
+
+result += " "+30*"="+"\n"+"\n"
+
+for compared_side_name, compared_side_keys in [('MASTER', keys_list_old), ('PR', keys_list_new)]:
+    result += compared_side_name
+    result += " KEYS NOT FOUND IN en-GB.txt"
     result += "\n"
 
-result += ("\n "+20*"-")
-result += ("```")
-result += ("\n\n\n")
+    for key in compared_side_keys:
+        result += "! "+key
+        result += "\n"
+        
+    result += "\n "+30*"-"+"\n"
+    result += "! "+str(len(compared_side_keys))+" keys\n"
+    result += " "+30*"="+"\n\n"
+
+
+result += "```"
+result += "\n\n\n"
 
 result_file.write(result)
 result_file.close()
